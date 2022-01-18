a9c6b3c0bfa8c40bb0756d56b2af66c81e3b81b6
Najmadiiriye <najmadiiriye11@gmail.com>
2022-01-18 11:42:38 +0000

    Scores Header made

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -5,14 +5,14 @@ function NavBar() {
   function addScore() {
     console.log("Add score button pressed");
   }
-
   return (
     <div>
-      <img src={logo} alt="logo" />
-      <button onClick={addScore}>+ Add Score</button>
-      <img src={UserIcon} alt="User Icon" />
+    <img src= {logo} alt = "logo"/>
+    <button onClick={addScore}>+Add Score</button>
+    <img src ={UserIcon} alt ="User Icon" />
     </div>
   );
 }
+
 
 export default NavBar;
--- client/src/components/ScoresHeader/index.js
+++ client/src/components/ScoresHeader/index.js
@@ -1,5 +1,13 @@
+import {useState} from "react";
+
+
 function ScoresHeader() {
-  return <div></div>;
+  const [averageScore, setAverageScore] = useState(0);
+   return <div>
+    <h1> Your Scores </h1>
+    <p> Hey there, here are your scores. Your average score is {averageScore}%. You're doing great, keep it up!</p>
+
+  </div>;
 }
 
 export default ScoresHeader;
