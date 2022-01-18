74ed3de232a7aaf04565dff6bd56cf1152be5412
ted.phillips <ted.phillips11@btinternet.com>
2022-01-18 09:57:41 +0000

    nav bar updated

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,5 +1,12 @@
 function NavBar() {
-  return <div></div>;
+  function addScore() {
+    console.log("Add score button pressed");
+  }
+  return (
+    <div>
+      <button onClick={addScore}>+Add Score</button>
+    </div>
+  );
 }
 
 export default NavBar;
