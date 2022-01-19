af330fd63c04e3200c17fe796ab6ea98a091f8cd
Simon Prato <simonprato11@gmail.com>
2022-01-19 10:39:29 +0000

    delete function

--- client/src/components/GraphScore/index.js
+++ client/src/components/GraphScore/index.js
@@ -1,29 +1,47 @@
-import "./style.css";
+import './style.css'
 
-function GraphScore({ date, topic, percentageScore }) {
-  let divWidth = percentageScore * 0.5;
+function GraphScore({ date, topic, percentageScore, key }) {
+  let divWidth = percentageScore * 0.5
 
-  function handleClick() {
-    console.log("You clicked delete");
+  async function deleteItem() {
+    console.log('id', key)
+    const response = await fetch(
+      `https://quiztrackerapp.herokuapp.com/scores/${key}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '',
+        },
+      }
+    )
+
+    const data = await response.json()
+    return data
+  }
+  async function handleClick() {
+    console.log('You clicked delete')
+    const res = await deleteItem()
+    console.log('response', res)
   }
 
   return (
-    <div class="graph-score">
-      <p class="light">{date}</p>
-      <p class="dark">{topic}</p>
+    <div class='graph-score'>
+      <p class='light'>{date}</p>
+      <p class='dark'>{topic}</p>
       <div
-        class="bar light"
+        class='bar light'
         style={{
           width: `${divWidth}vw`,
-          height: "20px",
+          height: '20px',
           backgroundColor: {},
         }}
       ></div>
-      <button class="delete-button dark" onClick={handleClick}>
+      <button class='delete-button dark' onClick={handleClick}>
         X
       </button>
     </div>
-  );
+  )
 }
 
-export default GraphScore;
+export default GraphScore
